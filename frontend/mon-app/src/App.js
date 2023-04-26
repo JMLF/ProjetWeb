c2a4dd80fac3866f98f8ac0c258ce7c755ee1863
marius <marius@MacBook-Air-de-Marius-2.local>
2023-04-26 09:47:14 +0200

    Ajout mui et appbar

--- frontend/mon-app/src/App.js
+++ frontend/mon-app/src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 import Header from './components/header_components';
 import getUser from './services/user_service';
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { Menu } from '@mui/icons-material';
+
 
 function App() {
   return (
     <div className="App">
-     <Header></Header>
+      <Appbarrr></Appbarrr>
+      <Header></Header>
      <br></br>
      <div className="ListUsers">
      <UserTile></UserTile>
@@ -54,11 +64,65 @@ function UserTile() {
   ));
 
   return (
-    <div className="UsersTiles">
-      <div>{Loading? freee : userList}</div>
-      <br></br>
+    <div>
+    <div style={{ position: "sticky", top: 0 }}>
+      <Table>
+        <TableHead>
+          <TableRow
+            style={{
+              backgroundColor: "#f5f5f5",
+              height: "35px"
+            }}
+          >
+            <TableCell>User Id</TableCell>
+            <TableCell>Surname</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Gender</TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
     </div>
+    <div style={{ height: "300px", overflow: "auto" }}>
+      <Table style={{ tableLayout: "fixed" }}>
+        <TableBody>
+          {data.map((user, index) => {
+            return (
+              <TableRow key={index}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.surname}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.civility.status}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  </div>
   ); 
 }
 
+
+function Appbarrr() {
+  return(
+    <AppBar>
+    <Toolbar>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+      >
+        <Menu />
+      </IconButton>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Gestion 
+      </Typography>
+      <Button color="inherit">Add</Button>
+      <Button color="inherit">Delete</Button>
+    </Toolbar>
+  </AppBar>
+  );
+}
 
